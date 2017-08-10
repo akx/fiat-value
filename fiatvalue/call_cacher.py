cf819646b77df3c8d86814dce8706633d47b834f
Aarni Koskela <akx@iki.fi>
2017-08-10 15:47:56 +0300

    Refactor cache

--- fiatvalue/call_cacher.py
+++ fiatvalue/call_cacher.py
@@ -4,28 +4,56 @@ import time
 import hashlib
 from functools import wraps
 
+
+class Expired(Exception):
+    pass
+
+
+class Cache:
+    def __init__(self, name, ttl=60):
+        self.shelf = shelve.open('%s.cache.shelf' % name)
+        self.ttl = ttl
+
+    def hash(self, value):
+        return hashlib.sha1(json.dumps(value, sort_keys=True).encode('utf-8')).hexdigest()
+
+    def hash_args_kwargs(self, args, kwargs):
+        return self.hash([args, list(sorted(kwargs.items()))])
+
+    def get(self, key):
+        itime, value = self.shelf[key]
+        lifetime = time.time() - itime
+        if lifetime > self.ttl:
+            raise Expired(lifetime)
+        return value
+
+    def set(self, key, value):
+        self.shelf[key] = (time.time(), value)
+
+    def retrieve_or_call(self, fn, args, kwargs):
+        hash = self.hash_args_kwargs(args, kwargs)
+        try:
+            return self.get(hash)
+        except (KeyError, Expired):
+            rv = fn(*args, **kwargs)
+            self.set(hash, rv)
+            return rv
+
+    def wrap_function(self, fn):
+        @wraps(fn)
+        def wrapped(*args, **kwargs):
+            return self.retrieve_or_call(fn, args, kwargs)
+
+        return wrapped
+
+
 class CallCacher:
     def __init__(self, name, target, ttl=60):
-        self.__shelf = shelve.open('%s.cache.shelf' % name)
+        self.__cache = Cache(name, ttl)
         self.__target = target
-        self.__ttl = ttl
 
     def __getattr__(self, name):
         obj = getattr(self.__target, name)
         if not callable(obj):
             return obj
-        return self.__wrap_fn(obj)
-
-    def __wrap_fn(self, fn):
-        @wraps(fn)
-        def wrapped(*args, **kwargs):
-            vals = [args, list(sorted(kwargs.items()))]
-            hash = hashlib.sha1(json.dumps(vals).encode('utf-8')).hexdigest()
-            rv = self.__shelf.get(hash)
-            if rv:
-                if time.time() - rv[0] < self.__ttl:
-                    return rv[1]
-            rv = fn(*args, **kwargs)
-            self.__shelf[hash] = (time.time(), rv)
-            return rv
-        return wrapped
+        return self.__cache.wrap_function(obj)
